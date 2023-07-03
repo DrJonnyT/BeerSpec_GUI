98dce69b4fd53b597712980849bda27dfe0668b1
Jonny Taylor <42604380+DrJonnyT@users.noreply.github.com>
2023-07-03 10:31:25 +0100

    Create settings class

---
+++ BeerSpec_GUI/SettingsClass.h
@@ -0,0 +1,17 @@
+#pragma once
+ref class SettingsClass
+{
+private:
+	int m_LEDR;
+	int m_LEDG;
+	int m_LEDB;
+	float m_GainExtR;
+	float m_GainExtG;
+	float m_GainExtB;
+	float m_GainScaR;
+	float m_GainScaG;
+	float m_GainScaB;
+	int m_NumSamples;
+
+};
+
