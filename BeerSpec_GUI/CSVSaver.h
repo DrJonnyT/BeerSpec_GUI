8e9df9066324e4927b1421c6cda5000a27cd9ae7
Jonny Taylor <42604380+DrJonnyT@users.noreply.github.com>
2023-07-04 16:54:25 +0100

    Add CSV saving function

--- BeerSpec_GUI/CSVSaver.h
+++ BeerSpec_GUI/CSVSaver.h
@@ -2,8 +2,13 @@
 #include <string>
 
 #using <System.dll>
+#include <fstream>
+#include "SettingsClass.h"
+#include "MeasClass.h"
 
 using namespace System;
+using namespace System::IO;
+
 
 
 
@@ -61,6 +66,57 @@ public:
     //	System::String^ get() { return m_FilePath; }
     //	void set(System::String^ value) { m_FilePath = value; }
     //}
+
+    //Function to actually save the data
+    void SaveDataToFile(SettingsClass^ settings, MeasClass^ meas)
+    {
+        // Check if the file exists
+        bool fileExists = File::Exists(m_FilePath);
+
+        // Open the file for appending or create a new file
+        StreamWriter^ writer = gcnew StreamWriter(m_FilePath, fileExists);
+
+        // Write the header row if the file is new
+        if (!fileExists)
+        {
+            //Header for time and settings
+            writer->Write("Time,LED_R,LED_G,LED_B,GainExtR,GainExtG,GainExtB,GainScaR,GainScaG,GainScaB");
+            writer->Write("MeasExtR,MeasExtG,MeasExtB,MeasExtR_std,MeasExtG_std,MeasExtB_std,MeasScaR,MeasScaG,MeasScaB,MeasScaR_std,MeasScaG_std,MeasScaB_std,Notes");
+            
+            writer->WriteLine();
+        }
+
+        // Write the time and settings
+        writer->Write(meas->MeasTime + ",");
+        writer->Write(settings->LEDR.ToString() + ",");
+        writer->Write(settings->LEDG.ToString() + ",");
+        writer->Write(settings->LEDB.ToString() + ",");
+        writer->Write(settings->GainExtR.ToString() + ",");
+        writer->Write(settings->GainExtG.ToString() + ",");
+        writer->Write(settings->GainExtB.ToString() + ",");
+        writer->Write(settings->GainScaR.ToString() + ",");
+        writer->Write(settings->GainScaG.ToString() + ",");
+        writer->Write(settings->GainScaB.ToString() + ",");
+        //Write the measured data
+        writer->Write(meas->MeasExtR.ToString() + ",");
+        writer->Write(meas->MeasExtG.ToString() + ",");
+        writer->Write(meas->MeasExtB.ToString() + ",");
+        writer->Write(meas->MeasExtR_std.ToString() + ",");
+        writer->Write(meas->MeasExtG_std.ToString() + ",");
+        writer->Write(meas->MeasExtB_std.ToString() + ",");
+        writer->Write(meas->MeasScaR.ToString() + ",");
+        writer->Write(meas->MeasScaG.ToString() + ",");
+        writer->Write(meas->MeasScaB.ToString() + ",");
+        writer->Write(meas->MeasScaR_std.ToString() + ",");
+        writer->Write(meas->MeasScaG_std.ToString() + ",");
+        writer->Write(meas->MeasScaB_std.ToString() + ",");
+        writer->Write("\"" + meas->Notes + "\"");
+
+        writer->WriteLine();
+
+        // Close the file
+        writer->Close();
+    }
 
 };
 
