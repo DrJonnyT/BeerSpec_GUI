b78df5aeddb8bba6f06abee21d236f4676d7365d
Jonny Taylor <42604380+DrJonnyT@users.noreply.github.com>
2023-07-05 17:20:16 +0100

    Remove template buttons

--- BeerSpec_GUI/Form1.h
+++ BeerSpec_GUI/Form1.h
@@ -3,8 +3,8 @@
 // uncomment to execute the rk1-utils:
 //    #include "rk1_Utils_demo.h"  // shows how the rk1-utils can be used
 
-#include "Header1.h"
-#include "Header2.h"
+//#include "Header1.h"
+//#include "Header2.h"
 #include "SettingsClass.h"
 #include "MeasClass.h"
 #include "CSVSaver.h"
@@ -124,9 +124,9 @@ namespace CppCLRWinFormsProject {
 
   //Default buttons etc from the example
   private: System::Windows::Forms::TextBox^ out_textBox;
-  private: System::Windows::Forms::TextBox^ in_textBox;
-  private: System::Windows::Forms::Button^ button_plus_1;
-  private: System::Windows::Forms::Button^ button_plus_2;
+
+
+
   private: System::ComponentModel::IContainer^ components;
 
 
@@ -207,9 +207,6 @@ private: System::Windows::Forms::Button^ btnSave;
     {
         this->components = (gcnew System::ComponentModel::Container());
         this->out_textBox = (gcnew System::Windows::Forms::TextBox());
-        this->in_textBox = (gcnew System::Windows::Forms::TextBox());
-        this->button_plus_1 = (gcnew System::Windows::Forms::Button());
-        this->button_plus_2 = (gcnew System::Windows::Forms::Button());
         this->labSettings = (gcnew System::Windows::Forms::Label());
         this->labLED = (gcnew System::Windows::Forms::Label());
         this->labGainExt = (gcnew System::Windows::Forms::Label());
@@ -275,37 +272,6 @@ private: System::Windows::Forms::Button^ btnSave;
         this->out_textBox->Name = L"out_textBox";
         this->out_textBox->Size = System::Drawing::Size(156, 74);
         this->out_textBox->TabIndex = 0;
-        // 
-        // in_textBox
-        // 
-        this->in_textBox->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
-        this->in_textBox->Location = System::Drawing::Point(570, 38);
-        this->in_textBox->Name = L"in_textBox";
-        this->in_textBox->Size = System::Drawing::Size(100, 20);
-        this->in_textBox->TabIndex = 1;
-        this->in_textBox->TextChanged += gcnew System::EventHandler(this, &Form1::in_textBox_TextChanged);
-        // 
-        // button_plus_1
-        // 
-        this->button_plus_1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
-        this->button_plus_1->Location = System::Drawing::Point(570, 68);
-        this->button_plus_1->Name = L"button_plus_1";
-        this->button_plus_1->Size = System::Drawing::Size(75, 23);
-        this->button_plus_1->TabIndex = 2;
-        this->button_plus_1->Text = L"plus 1";
-        this->button_plus_1->UseVisualStyleBackColor = true;
-        this->button_plus_1->Click += gcnew System::EventHandler(this, &Form1::button_plus_1_Click);
-        // 
-        // button_plus_2
-        // 
-        this->button_plus_2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
-        this->button_plus_2->Location = System::Drawing::Point(570, 97);
-        this->button_plus_2->Name = L"button_plus_2";
-        this->button_plus_2->Size = System::Drawing::Size(75, 23);
-        this->button_plus_2->TabIndex = 3;
-        this->button_plus_2->Text = L"plus 2";
-        this->button_plus_2->UseVisualStyleBackColor = true;
-        this->button_plus_2->Click += gcnew System::EventHandler(this, &Form1::button_plus_2_Click);
         // 
         // labSettings
         // 
@@ -778,9 +744,6 @@ private: System::Windows::Forms::Button^ btnSave;
         this->Controls->Add(this->labGainExt);
         this->Controls->Add(this->labLED);
         this->Controls->Add(this->labSettings);
-        this->Controls->Add(this->button_plus_2);
-        this->Controls->Add(this->button_plus_1);
-        this->Controls->Add(this->in_textBox);
         this->Controls->Add(this->out_textBox);
         this->Name = L"Form1";
         this->Text = L"BeerSpec GUI";
@@ -809,17 +772,6 @@ private: System::Windows::Forms::Button^ btnSave;
    // You can call the functions at a button click. If you prefer, 
     // you can call them by clicking a menu item.
 
-  private: System::Void button_plus_1_Click(System::Object^ sender, System::EventArgs^ e)
-  {
-    int n = Convert::ToInt32(in_textBox->Text);
-    int result = N_header_1::plus_1(n);
-    out_textBox->AppendText(String::Format("plus_1({0})={1}\r\n", n, result));
-  }
-
-  private: System::Void button_plus_2_Click(System::Object^ sender, System::EventArgs^ e)
-  {
-    N_header_2::plus_2_Click(in_textBox, out_textBox);
-  }
 
 
   private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
